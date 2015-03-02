cb627dc6ada2826bf9f49adcef4cac35e96340bb
Federico Knüssel <fknussel@gmail.com>
2015-03-02 20:24:22 -0300

    Have the timeline update when the app starts no matter where the data comes from (db or api)

--- app/src/main/java/com/fknussel/news/LocalNewsFragment.java
+++ app/src/main/java/com/fknussel/news/LocalNewsFragment.java
@@ -43,8 +43,6 @@ public class LocalNewsFragment extends Fragment {
 
         // Needs to be final so that it can be accessed from the onItemClickListener
         final NewsAdapter newsAdapter = new NewsAdapter(getActivity());
-        
-        ArrayList<Post> posts = new ArrayList<>();
 
         // if we have cached data, do not query the API
         DbHelper dbHelper = new DbHelper(getActivity());
@@ -52,58 +50,23 @@ public class LocalNewsFragment extends Fragment {
 
         // Cursor to query the database
         String postQuery = "SELECT * FROM " + PostContract.TABLE +
-                " ORDER BY " + PostContract.DEFAULT_SORT; 
+                " ORDER BY " + PostContract.DEFAULT_SORT;
         Cursor postCursor = db.rawQuery(postQuery, null);
 
         if (postCursor != null && postCursor.getCount() > 0) {
-            if (postCursor.moveToFirst()) {
-                do {
-                    int id = postCursor.getInt(postCursor.getColumnIndex(PostContract.Column.ID));
-                    String title = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.TITLE));
-                    String excerpt = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.EXCERPT));
-                    String body = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.BODY));
-                    String category = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.CATEGORY));
-                    String date = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.DATE));
+        
+            ArrayList<Post> posts = fetchPostsFromDb(newsAdapter);
+            
+            newsAdapter.addAll(posts);
+            newsAdapter.notifyDataSetChanged();
+            
+            Toast.makeText(getActivity(), "timeline data fetched from DB", Toast.LENGTH_SHORT).show();
+            
+            // here we should query the API again to fetch just
+            // the latest posts and somehow merge them into
+            // our adapter existing data
+            fetchPostsFromApi(newsAdapter, 1);
 
-                    // Fetch the media items associated to the current post (if any)
-                    ArrayList<Media> media = new ArrayList<>();
-
-                    String mediaQuery = "SELECT * FROM " + MediaContract.TABLE +
-                            " WHERE " + MediaContract.Column.POST_ID + " = " + id +
-                            " ORDER BY " + MediaContract.DEFAULT_SORT;
-
-                    Cursor mediaCursor = db.rawQuery(mediaQuery, null);
-
-                    if (mediaCursor.moveToFirst()) {
-                        do {
-                            int mediaId = mediaCursor.getInt(mediaCursor.getColumnIndex(MediaContract.Column.ID));
-                            String url = mediaCursor.getString(mediaCursor.getColumnIndex(MediaContract.Column.URL));
-                            String type = mediaCursor.getString(mediaCursor.getColumnIndex(MediaContract.Column.TYPE));
-
-                            media.add(new Media(mediaId, id, type, url));
-                        } while (mediaCursor.moveToNext());
-
-                        mediaCursor.close();
-                    }
-
-                    // Add the post to the adapter
-                    Post post = new Post(id, title, excerpt, body, category, date, media);
-                    posts.add(post);
-
-                } while (postCursor.moveToNext());
-
-                postCursor.close();
-                
-                newsAdapter.addAll(posts);
-
-                Toast.makeText(getActivity(), "timeline data fetched from DB", Toast.LENGTH_SHORT).show();
-                
-                // here we should query the API again to fetch just
-                // the latest posts and somehow merge them into
-                // our adapter existing data
-                // fetchPostsFromApi(newsAdapter, 1);
-
-            }
         } else {
             // query the API
             fetchPostsFromApi(newsAdapter, 1);
@@ -140,11 +103,9 @@ public class LocalNewsFragment extends Fragment {
     }
     
     private void fetchPostsFromApi(final NewsAdapter newsAdapter, int paginationIndex) {
-        ApiClient.getApiInterface().getPosts(1, new Callback<List<Post>>() {
+        ApiClient.getApiInterface().getPosts(paginationIndex, new Callback<List<Post>>() {
             @Override
             public void success(List<Post> posts, Response response) {
-                newsAdapter.addAll(posts);
-                newsAdapter.notifyDataSetChanged();
 
                 // Cache the data
                 DbHelper dbHelper = new DbHelper(getActivity());
@@ -184,6 +145,12 @@ public class LocalNewsFragment extends Fragment {
                     Log.d(TAG, "Failed to fetch news feed");
                     e.printStackTrace();
                 }
+                
+                ArrayList<Post> updatedListOfPosts = fetchPostsFromDb(newsAdapter);
+                newsAdapter.addAll(updatedListOfPosts);
+                newsAdapter.notifyDataSetChanged();
+                
+                Toast.makeText(getActivity(), "updated list of posts stored into db", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -191,5 +158,59 @@ public class LocalNewsFragment extends Fragment {
                 Log.d(TAG, error.getMessage());
             }
         });
+    }
+    
+    private ArrayList<Post> fetchPostsFromDb(final NewsAdapter newsAdapter) {
+        ArrayList<Post> posts = new ArrayList<>();
+
+        // if we have cached data, do not query the API
+        DbHelper dbHelper = new DbHelper(getActivity());
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // Cursor to query the database
+        String postQuery = "SELECT * FROM " + PostContract.TABLE +
+                " ORDER BY " + PostContract.DEFAULT_SORT;
+        Cursor postCursor = db.rawQuery(postQuery, null);
+
+        
+            while (postCursor.moveToNext()) {
+                int id = postCursor.getInt(postCursor.getColumnIndex(PostContract.Column.ID));
+                String title = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.TITLE));
+                String excerpt = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.EXCERPT));
+                String body = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.BODY));
+                String category = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.CATEGORY));
+                String date = postCursor.getString(postCursor.getColumnIndex(PostContract.Column.DATE));
+
+                // Fetch the media items associated to the current post (if any)
+                ArrayList<Media> media = new ArrayList<>();
+
+                String mediaQuery = "SELECT * FROM " + MediaContract.TABLE +
+                        " WHERE " + MediaContract.Column.POST_ID + " = " + id +
+                        " ORDER BY " + MediaContract.DEFAULT_SORT;
+
+                Cursor mediaCursor = db.rawQuery(mediaQuery, null);
+
+                if (mediaCursor.moveToFirst()) {
+                    do {
+                        int mediaId = mediaCursor.getInt(mediaCursor.getColumnIndex(MediaContract.Column.ID));
+                        String url = mediaCursor.getString(mediaCursor.getColumnIndex(MediaContract.Column.URL));
+                        String type = mediaCursor.getString(mediaCursor.getColumnIndex(MediaContract.Column.TYPE));
+
+                        media.add(new Media(mediaId, id, type, url));
+                    } while (mediaCursor.moveToNext());
+
+                    mediaCursor.close();
+                }
+
+                // Add the post to the adapter
+                Post post = new Post(id, title, excerpt, body, category, date, media);
+                posts.add(post);
+
+            }
+
+            postCursor.close();
+        
+        
+        return posts;
     }
 }
